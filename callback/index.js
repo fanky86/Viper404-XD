741e98dad62fa9db3b5cfe646f21ab835fa0cc0e
Tanpa Nama <106883384+fanky86@users.noreply.github.com>
2024-11-25 11:44:17 +0700

    Create index.js

---
+++ callback/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// Endpoint untuk callback
+app.get("/callback", (req, res) => {
+    const authCode = req.query.code; // Mendapatkan kode otorisasi dari query
+    if (authCode) {
+        res.send(`Kode Otorisasi: ${authCode}`);
+    } else {
+        res.send("Kode otorisasi tidak ditemukan!");
+    }
+});
+
+// Jalankan server
+app.listen(3000, () => {
+    console.log("Server berjalan di http://localhost:3000");
+});
+
+module.exports = app;
